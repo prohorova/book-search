404a3e6009feb52b52d8b5313e8f9ae9787524be
unknown <prohorova@prohorova.haulmont.com>
2014-10-24 14:07:40 +0400

    commit

--- js/controllers.js
+++ js/controllers.js
@@ -4,157 +4,147 @@
     app.controller("MainController", ["$scope", "merchants", "campusbookService", "$filter", "$rootScope",
 
         function ($scope, merchants, campusbookService, $filter, $rootScope) {
-                                      
+
             $scope.mainModel = {
-                isbn: undefined,  
+                isbn: undefined,
                 isbn13: undefined,
                 title: undefined,
                 minimalPriceToPrint: 5,
                 enteredPrice: undefined,
                 automaticPrint: false,
                 requestProcessing: false,
-                maxPrice: 0,
+                maxPrice: undefined,
                 offers: [],
                 errors: []
             };
-            
+
             $scope.merchants = merchants;
-                   
-            var resetErrors = function() {
+
+            var resetErrors = function () {
                 $scope.mainModel.errors = [];
             };
-    
-            var resetOffers = function() {
+
+            var resetOffers = function () {
                 $scope.mainModel.offers = [];
             };
-            
-            var findMerchant = function(id) {
-                var i, l, merchant;
-                for (i = 0, l = $scope.merchants.length; i < l; i++) {
-                    if ($scope.merchants[i]["id"] == id) {
-                        merchant = $scope.merchants[i]; 
-                        break;
-                    }    
-                }
-                return merchant;
-            };
-            
-            var processResponse = function(data) {
+
+            var processResponse = function (data) {
                 var merchant, i, l;
-                
+
                 $scope.mainModel.requestProcessing = false;
-                
+
                 if (data.success) {
                     if (data.isbn13 && data.title) {
                         $scope.mainModel.isbn13 = data.isbn13;
-                        $scope.mainModel.title = data.title;    
-                    }
-                    else {
-                        $scope.mainModel.errors.push("Couldn't retrieve book information");  
+                        $scope.mainModel.title = data.title;
+                    } else {
+                        $scope.mainModel.errors.push("Couldn't retrieve book information");
                         return;
-                    }
+                    };
                     if (data.offers.length) {
-                        _.forEach(data.offers, function(offer) {
-                            merchant = findMerchant(offer["merchant_id"]);
-                            if (merchant && merchant.use) {
-                                $scope.mainModel.offers.push(offer);   
-                            }
-                        });   
+                        $scope.mainModel.offers = _.filter(data.offers, function (offer) {
+                            return _.find(merchants, function (merchant) {
+                                return merchant.id == offer.merchant_id && merchant.use;
+                            })
+                        })
                         if (!$scope.mainModel.offers.length) {
-                            $scope.mainModel.errors.push("No results found"); 
+                            $scope.mainModel.errors.push("No results found");
                             return;
                         }
-                        $scope.mainModel.maxPrice = _.max($scope.mainModel.offers, function(offer){ return offer.price }).price; 
+                        $scope.mainModel.maxPrice = _.max($scope.mainModel.offers, function (offer) {
+                            return offer.price
+                        }).price;
                     } else {
-                        $scope.mainModel.errors.push("No results found");     
-                    }
+                        $scope.mainModel.errors.push("No results found");
+                    };
                     $rootScope.$broadcast("searchFinished", $scope.mainModel.isbn13, $scope.mainModel.title, $scope.mainModel.offers, $scope.printOnStart());
                 } else {
                     if (data.errors.length) {
-                        $scope.mainModel.errors = data.errors;        
+                        $scope.mainModel.errors = data.errors;
                     } else {
                         $scope.mainModel.errors.push("Unknown error occured");
-                    } 
-                }    
+                    }
+                };
             };
-    
+
             $scope.setMinimalPrice = function (price) {
                 $scope.mainModel.minimalPriceToPrint = price;
             };
 
             $scope.searchBooks = function (isbn) {
                 var INTEGER_REGEXP = /^\-?\d+$/;
-                
+
                 resetErrors();
-                if (isbn && INTEGER_REGEXP.test(isbn) && (isbn.length === 10 || isbn.length === 13)) { 
-                    resetOffers(); 
+                if (isbn && INTEGER_REGEXP.test(isbn) && (isbn.length === 10 || isbn.length === 13)) {
+                    resetOffers();
                     $scope.mainModel.requestProcessing = true;
                     campusbookService.searchBookPrices($scope.mainModel.isbn, processResponse);
                 } else {
                     $scope.mainModel.errors.push("Isbn must contain 10 or 13 digits");
-                }
+                };
             };
-            
-            $scope.showResults = function() {
+
+            $scope.showResults = function () {
                 return !!$scope.mainModel.offers.length;
             };
-            
-            $scope.getFormattedIsbn = function() {
-                return $filter("isbn")($scope.mainModel.isbn13);   
+
+            $scope.getFormattedIsbn = function () {
+                return $filter("isbn")($scope.mainModel.isbn13);
             };
-            
-            $scope.printOnStart = function() {
-                return ($scope.mainModel.maxPrice >= $scope.mainModel.minimalPriceToPrint) && $scope.mainModel.automaticPrint;    
+
+            $scope.printOnStart = function () {
+                return ($scope.mainModel.maxPrice >= $scope.mainModel.minimalPriceToPrint) && $scope.mainModel.automaticPrint;
             };
     }]);
-    
-    app.controller("logsController", ["$scope", "loggingService", "logsOnPage", function($scope, loggingService, logsOnPage) {
-        var offset = 0,
-            count = logsOnPage;
-        
-        $scope.logsModel = {
-            logs: [],
-            total: 0,
-            moreLogsAvailable: false,
-            requestProcessing: false
-        };
-        
-        var checkIfMoreLogsAvailable = function() {
-            $scope.logsModel.moreLogsAvailable = $scope.logsModel.logs.length < $scope.logsModel.total;        
-        };
-        
-        var processLogsListResponse = function(data) {
-            $scope.logsModel.requestProcessing = false
-            if (data.logs) {
-                $scope.logsModel.logs = $scope.logsModel.logs.concat(data.logs);    
-            }
-            offset = $scope.logsModel.logs.length;
-            $scope.logsModel.total = data.total;
-            checkIfMoreLogsAvailable();
-        }
-        
-        var processLogEntryCreationResponse = function(data) {
-            if (data.newFile) {
-                $scope.logsModel.logs.pop();
-                $scope.logsModel.logs.unshift(data.newFile); 
-                $scope.logsModel.total++;
+
+    app.controller("logsController", ["$scope", "loggingService", "logsOnPage",
+        function ($scope, loggingService, logsOnPage) {
+            var offset = 0,
+                count = logsOnPage;
+
+            $scope.logsModel = {
+                logs: [],
+                total: 0,
+                moreLogsAvailable: false,
+                requestProcessing: false
+            };
+
+            var checkIfMoreLogsAvailable = function () {
+                $scope.logsModel.moreLogsAvailable = $scope.logsModel.logs.length < $scope.logsModel.total;
+            };
+
+            var processLogsListResponse = function (data) {
+                $scope.logsModel.requestProcessing = false
+                if (data.logs) {
+                    $scope.logsModel.logs = $scope.logsModel.logs.concat(data.logs);
+                }
+                offset = $scope.logsModel.logs.length;
+                $scope.logsModel.total = data.total;
                 checkIfMoreLogsAvailable();
-            }
-        }
-        
-        loggingService.getLogs(offset, count, processLogsListResponse); 
-        
-        $scope.getMoreLogs = function() {
-            $scope.logsModel.requestProcessing = true,
-            loggingService.getLogs(offset, count, processLogsListResponse);     
-        }
-        
-        $scope.noLogs = function() {
-            return !($scope.logsModel.logs && $scope.logsModel.logs.length);
-        }
-        
-        $scope.$on("searchFinished", function(event, isbn, title, offers, isPrintedOnStart) {
-            loggingService.createLogEntry(isbn, title, offers, isPrintedOnStart, processLogEntryCreationResponse);         
-        })
+            };
+
+            var processLogEntryCreationResponse = function (data) {
+                if (data.newFile) {
+                    $scope.logsModel.logs.pop();
+                    $scope.logsModel.logs.unshift(data.newFile);
+                    $scope.logsModel.total++;
+                    checkIfMoreLogsAvailable();
+                }
+            };
+
+            loggingService.getLogs(offset, count, processLogsListResponse);
+
+            $scope.getMoreLogs = function () {
+                $scope.logsModel.requestProcessing = true,
+                loggingService.getLogs(offset, count, processLogsListResponse);
+            };
+
+            $scope.noLogs = function () {
+                return !($scope.logsModel.logs && $scope.logsModel.logs.length);
+            };
+
+            $scope.$on("searchFinished", function (event, isbn, title, offers, isPrintedOnStart) {
+                loggingService.createLogEntry(isbn, title, offers, isPrintedOnStart, processLogEntryCreationResponse);
+            });
     }]);
-})()
+})();
--- js/services.js
+++ js/services.js
@@ -1,80 +1,79 @@
 (function () {
-        var app = angular.module("BookingApp");
-        app.value("logsOnPage", 2);
-        app.value("logsDir", "logs/");
+    var app = angular.module("BookingApp");
+    app.value("logsOnPage", 5);
+    app.value("logsDir", "logs/");
 
-        app.value("merchants", [
-            {
-                "id": 24,
-                "name": "Amazon Mkt",
-                "use": true
-            },
-            {
-                "id": 23,
-                "name": "Alibris.com",
-                "use": true
-            },
-            {
-                "id": 2022,
-                "name": "AbeBooks.com",
-                "use": true
-            },
-            {
-                "id": 19,
-                "name": "Half.com",
-                "use": true
-            },
-            {
-                "id": 30,
-                "name": "Barnes & Noble Marketplace",
-                "use": true
-            },
-        ]);
+    app.value("merchants", [
+        {
+            "id": 24,
+            "name": "Amazon Mkt",
+            "use": true
+        },
+        {
+            "id": 23,
+            "name": "Alibris.com",
+            "use": true
+        },
+        {
+            "id": 2022,
+            "name": "AbeBooks.com",
+            "use": true
+        },
+        {
+            "id": 19,
+            "name": "Half.com",
+            "use": true
+        },
+        {
+            "id": 30,
+            "name": "Barnes & Noble Marketplace",
+            "use": true
+        },
+    ]);
 
-        app.factory("campusbookService", ["$http",
+    app.factory("campusbookService", ["$http",
         function ($http) {
-                var url = "getBooksList.php";
-                return {
-                    searchBookPrices: function (isbn, callback) {
-                        return $http.get(url, {
-                            params: {
-                                isbn: isbn,
-                            }
-                        }).then(function (resp) {
-                            callback(resp.data);
-                        });
-
-                    }
-                }
-        }]);
-
-        app.factory("loggingService", ["$http", "logsDir",
-                function ($http, logsDir) {
-                    var getLogsUrl = "getLogs.php",
-                        createLogEntryUrl = "createLogEntry.php";
-                    return {
-                        getLogs: function (offset, count, callback) {
-                            $http.get(getLogsUrl, {
-                                params: {
-                                    offset: offset,
-                                    count: count,
-                                    logsDir: logsDir
-                                }
-                            }).then(function (resp) {
-                                callback(resp.data);
-                            })
-                        },
-                        createLogEntry: function (isbn, title, offers, isPrinted, callback) {
-                            $http.post(createLogEntryUrl, {
-                                isbn: isbn,
-                                title: title,
-                                offers: offers,
-                                isPrinted: isPrinted.toString(),
-                                logsDir: logsDir
-                            }).then(function (resp) {
-                                callback(resp.data);
-                            });
+            var url = "getBooksList.php";
+            return {
+                searchBookPrices: function (isbn, callback) {
+                    return $http.get(url, {
+                        params: {
+                            isbn: isbn,
                         }
-                    }
-                }]);
-        })()
+                    }).then(function (resp) {
+                        callback(resp.data);
+                    });
+                }
+            }
+    }]);
+
+    app.factory("loggingService", ["$http", "logsDir",
+                function ($http, logsDir) {
+            var getLogsUrl = "getLogs.php",
+                createLogEntryUrl = "createLogEntry.php";
+            return {
+                getLogs: function (offset, count, callback) {
+                    $http.get(getLogsUrl, {
+                        params: {
+                            offset: offset,
+                            count: count,
+                            logsDir: logsDir
+                        }
+                    }).then(function (resp) {
+                        callback(resp.data);
+                    });
+                },
+                createLogEntry: function (isbn, title, offers, isPrinted, callback) {
+                    $http.post(createLogEntryUrl, {
+                        isbn: isbn,
+                        title: title,
+                        offers: offers,
+                        isPrinted: isPrinted.toString(),
+                        logsDir: logsDir
+                    }).then(function (resp) {
+                        callback(resp.data);
+                    });
+                }
+            }
+    }]);
+})();
